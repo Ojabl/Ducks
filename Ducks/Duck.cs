41eaaac15375cda401dbde233dea4efb3d2f91e9
osajab10@gmail.com <osajab10@gmail.com>
2022-09-15 15:38:47 +0200

    Add project files.

---
+++ Ducks/Duck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    class Duck
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+    }
+
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Loon,
+    }
+}
